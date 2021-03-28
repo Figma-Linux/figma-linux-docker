0de9b242c0cb243b36c9951633620a31be9d0a4f
Chugunov Roman <Zebs-BMK@yandex.ru>
2021-03-29 01:30:57 +0300

    update Dockerfile_arm64v8

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 # docker build -t 4tqrgqe5yrgfd/figma-linux-docker-image-arm .
 # docker buildx build --platform linux/arm64 -t 4tqrgqe5yrgfd/figma-linux-docker-image --push .
 
-docker buildx build --platform linux/arm64 -t 4tqrgqe5yrgfd/figma-linux-docker-image-arm --load .
+docker buildx build --platform linux/arm64 -t 4tqrgqe5yrgfd/figma-linux-docker-image-arm --load -f ./Dockerfile_arm64v8 .
